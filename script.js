700bb81151bb1021507be5644a16dd7ca32e7ff0
Rabuu <n23493208@gmail.com>
2026-07-19 23:43:36 +0600

    Add functions to show letters and send love alert

---
+++ script.js
@@ -0,0 +1,11 @@
+function showLetter() {
+  document.getElementById("letter").classList.remove("hidden");
+}
+
+function showFinal() {
+  document.getElementById("final").classList.remove("hidden");
+}
+
+function heartRain() {
+  alert("❤️ Love sent to Abid ❤️");
+}
